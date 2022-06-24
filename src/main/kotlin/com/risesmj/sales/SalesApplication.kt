17b4630b47a1cc678e0de08b902d0b45769d958e
MJ <matheusjcunha@gmail.com>
2022-06-24 20:09:51 -0300

    Initial commit

---
+++ src/main/kotlin/com/risesmj/sales/SalesApplication.kt
@@ -0,0 +1,11 @@
+package com.risesmj.sales
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SalesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SalesApplication>(*args)
+}
